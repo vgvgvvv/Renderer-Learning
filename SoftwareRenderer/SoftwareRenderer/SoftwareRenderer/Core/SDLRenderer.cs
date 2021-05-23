4f9d786690700fb7fdaa9a8cc1f25e80313de8ac
resetoter <vgvgvvvdc@gmail.com>
2021-05-24 07:06:26 +0800

    使用rendertarget

--- SoftwareRenderer/SoftwareRenderer/SoftwareRenderer/Core/SDLRenderer.cs
+++ SoftwareRenderer/SoftwareRenderer/SoftwareRenderer/Core/SDLRenderer.cs
@@ -8,6 +8,7 @@ namespace SoftwareRenderer.Core
     public class SDLRenderer : IRenderer
     {
         public IntPtr Renderer { get; }
+        private IntPtr RenderTargetTexture;
         public int Width { get; }
         public int Height { get; }
 
@@ -20,6 +21,8 @@ namespace SoftwareRenderer.Core
             Renderer = renderer;
             Width = width;
             Height = height;
+            RenderTargetTexture = SDL.SDL_CreateTexture(Renderer, SDL.SDL_PIXELFORMAT_RGB888,
+                (int) SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, width, height);
             device = new SoftwareRenderDevice(width, height);
 
         }
@@ -44,7 +47,7 @@ namespace SoftwareRenderer.Core
             SDL.SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 0);
             SDL.SDL_RenderClear(Renderer);
 
-
+            SDL.SDL_SetRenderTarget(Renderer, RenderTargetTexture);
             var frameBuffer = device.Render();
             var lastColor = Color.black;
             for (int x = 0; x < Width; x++)
@@ -52,19 +55,20 @@ namespace SoftwareRenderer.Core
                 for (int y = 0; y < Height; y++)
                 {
                     var color = frameBuffer[Width - x - 1, y];
-                    if (color.r != lastColor.r || 
-                        color.g != lastColor.g || 
-                        color.b != lastColor.b || 
-                        color.a != lastColor.a)
+                    if (Math.Abs(color.r - lastColor.r) > 0.0001f || 
+                        Math.Abs(color.g - lastColor.g) > 0.0001f || 
+                        Math.Abs(color.b - lastColor.b) > 0.0001f || 
+                        Math.Abs(color.a - lastColor.a) > 0.0001f)
                     {
                         SDL.SDL_SetRenderDrawColor(Renderer, (byte)(color.r * 255), (byte)(color.g * 255), (byte)(color.b * 255), 255);
                         lastColor = color;
                     }
                     SDL.SDL_RenderDrawPoint(Renderer, x, y);
-                    
                 }
             }
             
+            SDL.SDL_SetRenderTarget(Renderer, IntPtr.Zero);
+            SDL.SDL_RenderCopy(Renderer, RenderTargetTexture, IntPtr.Zero, IntPtr.Zero);
             
             SDL.SDL_RenderPresent(Renderer);
         }
