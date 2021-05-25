e312bc4cca8f719b10f80a5c370b8b5d89cbbd5e
dieseldong <dieseldong@tencent.com>
2021-05-25 11:05:18 +0800

    调整目录结构

---
+++ SoftwareRenderer/SoftwareRenderer/SoftwareRenderer/Engine/Component/ObjMeshRenderer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using System.Reflection;
+using MathLib;
+using SoftwareRenderer.Render;
+
+namespace SoftwareRenderer.Component
+{
+    public class ObjMeshRenderer : MeshRenderer
+    {
+         public ObjMeshRenderer(string fileName)
+        {
+            
+        }
+    }
+}
