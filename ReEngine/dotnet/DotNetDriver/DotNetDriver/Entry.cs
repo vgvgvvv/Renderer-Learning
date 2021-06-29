6386a9354a32b862255bb3562b7b3aadc9c2781c
dieseldong <dieseldong@tencent.com>
2021-06-30 00:37:44 +0800

    更新

--- ReEngine/dotnet/DotNetDriver/DotNetDriver/Entry.cs
+++ ReEngine/dotnet/DotNetDriver/DotNetDriver/Entry.cs
@@ -2,6 +2,13 @@
 {
     public class Entry
     {
-        
+		public virtual void OnInit() { }
+        public virtual void OnPreUpdate() { }
+        public virtual void OnUpdate() { }
+        public virtual void OnLateUpdate() { }
+        public virtual void OnBeforeRender() { }
+        public virtual void OnRender() { }
+        public virtual void OnAfterRender() { }
+        public virtual void OnShutDown() { }
     }
 }
