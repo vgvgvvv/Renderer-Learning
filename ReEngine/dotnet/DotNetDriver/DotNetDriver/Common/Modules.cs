91b688ba4aad735631a480f3f6ae3a29088bec67
dieseldong <dieseldong@tencent.com>
2021-07-01 17:53:48 +0800

    支持dotnet调用

---
+++ ReEngine/dotnet/DotNetDriver/DotNetDriver/Common/Modules.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace DotNetDriver.Common
+{
+    public class Modules
+    {
+        const uint LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool SetDefaultDllDirectories(uint DirectoryFlags);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern int AddDllDirectory(string NewDirectory);
+
+        public static void Initialize()
+        {
+            var downloadDirectory = Environment.CurrentDirectory;
+
+            // Indicate to search libraries in
+            // - application directory
+            // - paths set using AddDllDirectory or SetDllDirectory
+            // - %windows%\system32
+            SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+
+            Console.WriteLine("Add Dll Directory {0}", downloadDirectory);
+
+            // Add the directory of the native dll
+            AddDllDirectory(downloadDirectory);
+
+        }
+    }
+}
--- ReEngine/dotnet/DotNetDriver/DotNetDriver/NativeEntry.cs
+++ ReEngine/dotnet/DotNetDriver/DotNetDriver/NativeEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Runtime.InteropServices;
+using DotNetDriver.Common;
+using TestOpenGLd.ImGui;
 
 namespace DotNetDriver
 {
@@ -8,55 +9,51 @@ namespace DotNetDriver
 
         public static int OnInit(IntPtr arg, int argLength)
         {
-            Console.WriteLine("Init");
+            Console.WriteLine("Init DotNet");
+            Modules.Initialize();
             return 0;
         }
 
         public static int OnPreUpdate(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnPreUpdate");
             return 0;
         }
 
         public static int OnUpdate(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnUpdate");
             return 0;
         }
 
         public static int OnLateUpdate(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnLateUpdate");
             return 0; 
         }
 
         public static int OnBeforeRender(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnBeforeRender");
             return 0;
         }
 
         public static int OnGUI(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnGUI");
+            bool open = true;
+            imgui.ShowDemoWindow(ref open);
             return 0;
         }
 
         public static int OnRender(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnRender");
             return 0;
         }
 
         public static int OnAfterRender(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnAfterRender");
             return 0;
         }
 
         public static int OnShutDown(IntPtr arg, int argLength)
         {
-            Console.WriteLine("OnShutDown");
+            Console.WriteLine("ShutDown DotNet");
             return 0;
         }
     }
