766d37ca33bf3084bc05ae4a1d7175fdf813fa8e
dieseldong <vgvgvvvdc@gmail.com>
2021-07-31 06:46:09 +0800

    删除

--- ReEngine/tools/temp/Output/DotNetAPId-symbols.cpp
+++
@@ -1,7 +0,0 @@
-#include <DotNetAPI_API.h>
-#include <NetLog.h>
-#include <ApplicationAPI.h>
-#include <new>
-
-NetLog& (NetLog::*_0)(NetLog&&) = &NetLog::operator=;
-ApplicationAPI& (ApplicationAPI::*_1)(ApplicationAPI&&) = &ApplicationAPI::operator=;
