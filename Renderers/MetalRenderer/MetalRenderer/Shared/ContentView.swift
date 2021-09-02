205c5c6a23fd43ff61c5108e9809dfbe0c335a43
dongchen <dongchen@tencent.com>
2021-09-02 23:26:09 +0800

    添加最基础的swift application 目录

---
+++ Renderers/MetalRenderer/MetalRenderer/Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 董宸 on 2021/9/2.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Renderers/MetalRenderer/MetalRenderer/Shared/MetalRendererApp.swift
@@ -0,0 +1,18 @@
+//
+//  MetalRendererApp.swift
+//  Shared
+//
+//  Created by 董宸 on 2021/9/2.
+//
+//
+
+import SwiftUI
+
+@main
+struct MetalRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
